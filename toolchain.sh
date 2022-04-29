c68547c07de31490cf3378e133f7cca3533de78c
Martin Kröning <mkroening@posteo.net>
2022-04-29 23:33:02 +0200

    WIP

--- toolchain.sh
+++ toolchain.sh
@@ -9,7 +9,7 @@
 set -e
 
 BUILDDIR=build
-CLONE_DEPTH="--depth=50"
+CLONE_DEPTH="--depth=1"
 PREFIX="$2"
 TARGET=$1
 NJOBS=-j"$(nproc)"
@@ -31,7 +31,7 @@ git clone $CLONE_DEPTH -b binutils-2_38 https://github.com/hermitcore/binutils.g
 fi
 
 if [ ! -d "gcc" ]; then
-git clone $CLONE_DEPTH -b releases/gcc-6.5.0 https://github.com/hermitcore/gcc.git
+git clone $CLONE_DEPTH https://github.com/hermitcore/gcc.git
 wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2 -O isl-0.15.tar.bz2
 tar jxf isl-0.15.tar.bz2
 mv isl-0.15 gcc/isl
@@ -56,7 +56,7 @@ if [ ! -d "tmp/binutils" ]; then
 mkdir -p tmp/binutils
 cd tmp/binutils
 ../../binutils/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-werror --disable-multilib --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --enable-tls --enable-lto --enable-plugin
-make -O $NJOBS
+make $NJOBS
 make install
 cd -
 fi
@@ -65,7 +65,7 @@ if [ ! -d "tmp/bootstrap" ]; then
 mkdir -p tmp/bootstrap
 cd tmp/bootstrap
 ../../gcc/configure --target=$TARGET --prefix=$PREFIX --without-headers --disable-multilib --with-isl --enable-languages=c,c++,lto --disable-nls --disable-shared --disable-libssp --disable-libgomp --enable-threads=posix --enable-tls --enable-lto --disable-symvers
-make -O $NJOBS all-gcc
+make $NJOBS all-gcc
 make install-gcc
 cd -
 fi
@@ -83,7 +83,7 @@ if [ ! -d "tmp/newlib" ]; then
 mkdir -p tmp/newlib
 cd tmp/newlib
 ../../newlib/configure --target=$TARGET --prefix=$PREFIX --disable-shared --disable-multilib --enable-lto --enable-newlib-hw-fp --enable-newlib-io-c99-formats --enable-newlib-multithread
-make -O $NJOBS
+make $NJOBS
 make install
 cd -
 fi
@@ -96,7 +96,7 @@ if [ ! -d "tmp/gcc" ]; then
 mkdir -p tmp/gcc
 cd tmp/gcc
 ../../gcc/configure --target=$TARGET --prefix=$PREFIX --with-newlib --with-isl --disable-multilib --without-libatomic --enable-languages=c,c++,go,fortran,lto --disable-nls --disable-shared --enable-libssp --enable-threads=posix --enable-libgomp --enable-tls --enable-lto --disable-symver
-make -O $NJOBS
+make $NJOBS
 make install
 cd -
 fi
